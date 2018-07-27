b1fa28c652354bd479b4fc7d3e6b17b5758f6c29
idepilogue <41766681+idepilogue@users.noreply.github.com>
2018-07-27 09:39:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/lineBot.php';
 $bot = new Linebot();
 $text = $bot->getMessageText();
 $bot->reply($text);
+$userId = $bot->getUserId();
 $bot->pushText($userId, 'Hello Simple Text!');
 
 
