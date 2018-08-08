cc71bc82426c37695a2fbb7931e69e398baa0b9a
idepilogue <41766681+idepilogue@users.noreply.github.com>
2018-08-08 15:28:11 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
-require_once __DIR__ . '/lineBot.php';
-
-$bot = new Linebot();
-$text = $bot->getMessageText();
-$bot->reply($text);
-$userId = $bot->getUserId();
-$bot->pushText($userId, 'Hello Simple Text!');
-
-?>
+echo "Khomsan K.";
+ ?>
