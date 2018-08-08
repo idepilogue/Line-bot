2306c4031efa93a2a77866effd54c01e3a07cc40
idepilogue <41766681+idepilogue@users.noreply.github.com>
2018-08-08 09:19:42 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,8 +2,7 @@
 
 class Setting {
 	public function getChannelAccessToken(){
-		$channelAccessToken = "kb/TNducgmz0+3HRUjP42bkLpr2M7pgt1CNRswKR3PqNBuIthoS5X3QZuQgaSpo9Ln91NHwayW/KQzYJCHbydMpJDVP3l/BkvoC57qYEBdj5By0l7oRAd2Cle6MX69qGboY6k5R5TvBDfhJx5BsqlgdB04t89/1O/w1cDnyilFU=
-";
+$channelAccessToken = "kb/TNducgmz0+3HRUjP42bkLpr2M7pgt1CNRswKR3PqNBuIthoS5X3QZuQgaSpo9Ln91NHwayW/KQzYJCHbydMpJDVP3l/BkvoC57qYEBdj5By0l7oRAd2Cle6MX69qGboY6k5R5TvBDfhJx5BsqlgdB04t89/1O/w1cDnyilFU=";
 		return $channelAccessToken;
 	}
 	public function getChannelSecret(){
